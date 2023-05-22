805942471adc7fb3dfd9206b0ce718a1c8e9910d
ernivani <tlindeker0@gmail.com>
2023-05-22 20:57:11 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,109 @@
+const express = require("express");
+const mysql2 = require("mysql2");
+const WebSocket = require("ws");
+const app = express();
+
+// initialize the connection to the database
+const connection = mysql2.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "ernicani",
+    database: "projetVo",
+});
+
+// connect to the database
+connection.connect((err) => {
+    if (err) throw err;
+    console.log("Connected to the database!");
+});
+
+// Initialize a WebSocket server instance
+const wss = new WebSocket.Server({ noServer: true });
+
+let clients = new Map(); // To hold all the active clients
+
+let nextId = 0; // To assign each connection a unique ID
+
+wss.on("connection", (ws, request) => {
+    const id = nextId++;
+    clients.set(id, ws); // Add the new connection to the clients map
+
+    console.log("Client connected :", id);
+
+    ws.on("message", async (message) => {
+        const data = JSON.parse(message);
+
+        if (data.type === "sentenceCheck") {
+            const query = "SELECT * FROM `vo_table` WHERE `phrase` = ?";
+            try {
+                const [rows, fields] = await connection
+                    .promise()
+                    .query(query, [data.payload]);
+
+                // if there are no rows, then the sentence is not in the database, and we assume it's correct
+                if (rows.length === 0) {
+                    ws.send(
+                        JSON.stringify({
+                            type: "sentenceCheck",
+                            payload: { correct: 1 },
+                        })
+                    );
+                } else {
+                    const result = rows[0]; // assume that there will be at most one row
+                    if (result.correct) {
+                        // the sentence is correct
+                        ws.send(
+                            JSON.stringify({
+                                type: "sentenceCheck",
+                                payload: { correct: 1 },
+                            })
+                        );
+                    } else {
+                        // the sentence is incorrect
+                        console.log(result);
+                        ws.send(
+                            JSON.stringify({
+                                type: "sentenceCheck",
+                                payload: {
+                                    correct: 0,
+                                    incorrect_word: result.mot_erreur,
+                                },
+                            })
+                        );
+                    }
+                }
+            } catch (err) {
+                console.log(err);
+            }
+        }
+        if (data.type === "sentenceCheckAnswer") {
+            // add to the database the sentence with the answerWord
+            console.log(data.payload);
+            const { sentence, answerWord } = data.payload;
+            const query =
+                "INSERT INTO `vo_table` (`Date`,`phrase`, `mot_erreur`, `correcte`) VALUES (NOW(), ?, ?, ?)";
+            try {
+                await connection
+                    .promise()
+                    .query(query, [sentence, answerWord, 0]);
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    });
+
+    ws.on("close", () => {
+        clients.delete(id); // Remove the connection from the clients map
+        console.log("Client disconnected :", id);
+    });
+});
+
+const server = app.listen(3000, () => {
+    console.log("Example app listening on port 3000!");
+});
+
+server.on("upgrade", (request, socket, head) => {
+    wss.handleUpgrade(request, socket, head, (ws) => {
+        wss.emit("connection", ws, request);
+    });
+});
---
+++ sr.js
@@ -0,0 +1,173 @@
+// ==UserScript==
+// @name           Apprentissage de l'orthographe
+// @namespace      http://example.com
+// @version        1.0
+// @description    Script Tampermonkey pour apprendre l'orthographe en français depuis Projet Voltaire.
+// @match          https://www.projet-voltaire.fr/*
+// @grant          none
+// ==/UserScript==
+
+(function () {
+	"use strict";
+
+	const selectors = {
+		sentence: ".sentence",
+		noMistakeButton: ".noMistakeButton",
+		nextButton: ".nextButton",
+		understoodButton: ".understoodButton",
+		buttonOk: ".buttonOk",
+		exitButton: ".exitButton",
+		answerWord: ".answerWord",
+		//
+	};
+
+	const ws = new WebSocket("wss://api.impin.fr/");
+	let understand = false;
+	let continueLearning = true; // Variable de contrôle pour continuer ou arrêter l'apprentissage
+
+	ws.onopen = () => {
+		console.log("WebSocket connection opened");
+	};
+
+	ws.onmessage = (event) => {
+		const result = JSON.parse(event.data);
+
+		if (result.type === "sentenceCheck") {
+			console.log("sentenceCheck :", result.payload);
+		}
+
+		if (result.payload.correct === 1) {
+			const noMistakeButton = document.querySelector(
+				selectors.noMistakeButton
+			);
+			// check if in the page there is a span with the title Mauvaise réponse
+			if (noMistakeButton) {
+				noMistakeButton.click();
+			}
+			// todo: attendre 0.2 secondes que la page se mette à jour
+
+			setTimeout(() => {
+				console.log("Timeout");
+			}, 200);
+
+			const spanAnswer = document.querySelector(
+				"span[title='Mauvaise réponse']"
+			);
+			if (spanAnswer) {
+				const answerWord = document.querySelector(selectors.answerWord);
+				if (answerWord) {
+					// send the sentence to the server with the answerWord
+					var sentence = document.querySelector(selectors.sentence);
+					if (sentence) {
+						sentence = sentence.innerText;
+					}
+
+					ws.send(
+						JSON.stringify({
+							type: "sentenceCheckAnswer",
+							payload: {
+								sentence: sentence,
+								answerWord: answerWord.innerText,
+							},
+						})
+					);
+				}
+			}
+			const nextButton = document.querySelector(selectors.nextButton);
+			if (nextButton) {
+				nextButton.click();
+			}
+		} else if (result.payload.correct === 0) {
+			var sentence = document.querySelector(selectors.sentence);
+			if (sentence) {
+				const errorNode = Array.from(sentence.childNodes).find(
+					(node) => {
+						// enlever tout les charactères spéciaux et les espaces
+						console.log(
+							result.payload.incorrect_word
+								.trim()
+								.replace(/^['’]/, "")
+								.split(/[\s-'’.,;:!?\u2026]/)[0]
+								.trim()
+						);
+						if (
+							node.innerText.trim() ===
+							result.payload.incorrect_word
+								.trim()
+								.replace(/^['’]/, "")
+								.split(/[\s-'’.,;:!?\u2026]/)[0]
+								.trim()
+						) {
+							return node;
+						}
+					}
+				);
+				if (errorNode) {
+					errorNode.click();
+					const nextButton = document.querySelector(
+						selectors.nextButton
+					);
+					if (nextButton) {
+						nextButton.click();
+					}
+				} else {
+					console.log("Error not found");
+				}
+			}
+		}
+	};
+
+	function learnSpelling() {
+		if (!continueLearning) {
+			return; // Arrêter l'exécution si continueLearning est false
+		}
+
+		var sentenceElement = document.querySelector(selectors.sentence);
+		if (sentenceElement) {
+			var sentence = sentenceElement.innerText;
+
+			ws.send(
+				JSON.stringify({
+					type: "sentenceCheck",
+					payload: sentence,
+				})
+			);
+		}
+
+		const understoodButton = document.querySelector(
+			selectors.understoodButton
+		);
+
+		if (understoodButton && !understand) {
+			understoodButton.click();
+			understand = true;
+		}
+
+		if (understand) {
+			var buttonOk = document.querySelectorAll(selectors.buttonOk); // select all elements with selector buttonOk
+
+			if (buttonOk) {
+				buttonOk.forEach((element) => {
+					element.click();
+				});
+			}
+
+			var exitButton = document.querySelector(selectors.exitButton);
+			if (exitButton) {
+				exitButton.click();
+			}
+
+			understand = false;
+		}
+	}
+
+	function main() {
+		if (document.readyState === "complete") {
+			learnSpelling();
+		}
+	}
+
+	window.addEventListener("load", function () {
+		setInterval(main, 3000);
+	});
+})();
